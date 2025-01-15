b21be690ca5d4b917edd1cbf7ac906a54d27a4e7
manasses.goncalves <manasses.goncalves@gmail.com>
2025-01-14 22:50:45 -0300

    fix: Rollback

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,18 @@
+import styles from './404.module.css';
+
 function Home() {
-    return <h1>ERRO 404 - ARRUMA ESSA MERDA</h1>
+  return (
+    <div className={styles.error404}>
+      <div className={styles.container}>
+        <h1>404</h1>
+        <h2>Página não encontrada</h2>
+        <p>Desculpe, mas a página que você está procurando não existe.</p>
+        <button className={styles['btn-voltar']} onClick={() => window.location.href = '/'}>
+          Voltar para a página inicial
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
